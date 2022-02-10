8c2f70aa9fde8f1d7d276bff554bbd8f394316c4
Yauma Dzikri Imany <yauma8610@gmail.com>
2022-02-10 14:51:46 +0700

    first init

---
+++ src/main/java/com/example/demouser/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.demouser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
